9f024b5e03e3af3afddf98bfeb97b07733e75234
sandmark <sandmark.m@gmail.com>
2016-01-03 03:53:41 +0900

    Fix redundant code of showing Rating

--- app/Main.hs
+++ app/Main.hs
@@ -71,7 +71,7 @@ putExitMessage r = do
   putStrLn "\nここでセットされた番号は保存されません"
   putStrLn "どこかにメモしておくことを忘れずに\n"
   putStrLn "--------------------------------------------------"
-  putStrLn $ show r
+  print r
   putStrLn "--------------------------------------------------\n"
   putStrLn "=== Enter キーで終了します ==="
   _ <- getLine
