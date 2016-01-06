d81e12fb8e3a875ea2cb67994ff0a4098dd0c37a
sandmark <sandmark.m@gmail.com>
2016-01-06 18:13:26 +0900

    Export some type constructors and functions.

--- src/Rating.hs
+++ src/Rating.hs
@@ -1,31 +1,44 @@
 module Rating
        (
          Rating(..)
+       , Index
+       , Lock
        , mapRating
        , emptyRating
+       , emptyIndex
+       , newIndex
        ) where
 
 import Data.List (intercalate)
 
-data Rating = Rating {dai    :: Maybe Int, kaour :: Maybe Int
-                     ,eirlys :: Maybe Int, elsie :: Maybe Int}
+data Rating = Rating {dai    :: Index, kaour :: Index
+                     ,eirlys :: Index, elsie :: Index}
+
+type Index = (Maybe Int, Lock)
+type Lock = Bool
+
+instance Show Rating where
+  show = prettyRating
 
 prettyRating :: Rating -> String
 prettyRating r = intercalate "\n" $ map tos l
   where l = [("ダイ", dai r),     ("アイリース", eirlys r)
             ,("カオル", kaour r), ("エルシィ", elsie r)]
-        tos (name, n) = "【" ++ name ++ "】\t\t=>\t" ++ case n of
-          Just n' -> show n'
-          Nothing -> "不明"
+        tos (name, i) = "【" ++ name ++ "】\t\t=>\t" ++ case i of
+          (Just n, True)   -> show n ++ "[固定]"
+          (Just n, _)      -> show n
+          (Nothing, _)     -> "不明"
 
-mapRating :: (Maybe Int -> Maybe Int) -> Rating -> Rating
+mapRating :: (Index -> Index) -> Rating -> Rating
 mapRating f (Rating {dai = d, eirlys = a, kaour = k, elsie = e}) =
   Rating {dai = f d, eirlys = f a, kaour = f k, elsie = f e}
 
+emptyIndex :: Index
+emptyIndex = (Nothing, False)
+
 emptyRating :: Rating
-emptyRating = Rating {dai    = Nothing, kaour = Nothing
-                     ,eirlys = Nothing, elsie = Nothing}
+emptyRating = Rating {dai    = emptyIndex, kaour = emptyIndex
+                     ,eirlys = emptyIndex, elsie = emptyIndex}
 
-
-instance Show Rating where
-  show = prettyRating
+newIndex :: Int -> Index
+newIndex n = (Just n, False)
