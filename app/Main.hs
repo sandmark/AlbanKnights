896717b92267a344fd31dabdc4e9b7ccb06005e6
sandmark <sandmark.m@gmail.com>
2016-01-08 18:32:50 +0900

    Fix to use AlbanKnights.unsafePick
    Delete unnecessary function `fromRight`

--- app/Main.hs
+++ app/Main.hs
@@ -92,7 +92,7 @@ npcKeywords key name index args = case args of
                     "'set' コマンドを使ってください。"
 
 wrappedPick :: String -> String -> Int -> Lock -> String
-wrappedPick name s i l = toString name $ fromRight $ AK.pick s (i-1)
+wrappedPick name s i l = toString name $ AK.unsafePick s (i-1)
   where toString name' keys =
           let locked = if l then " [固定]" else "\t"
           in "【" ++ name' ++ "】\t" ++ show i ++ locked ++ "\t" ++
@@ -191,10 +191,6 @@ npcNames = [("d","dai")
            ,("erusixi", "elsie")
            ,("erusili", "elsie")
            ]
-
-fromRight :: Either a b -> b
-fromRight (Right x) = x
-fromRight (Left _)  = error "fromRight: Argument takes from 'Left _'"
 
 logo :: String
 logo =
