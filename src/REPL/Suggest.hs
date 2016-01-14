48b2c7c26f45a1f3ab11df767528f604377ddc9d
sandmark <sandmark.m@gmail.com>
2016-01-15 08:36:58 +0900

    Fix argument of `pickOne` to zero-index

--- src/REPL/Suggest.hs
+++ src/REPL/Suggest.hs
@@ -39,4 +39,5 @@ toLine _ name (_,_,[]) = printf "【%s】\tストックが空です" name
 toLine npc name (_,_,keys) = case findKeyIndices npc keys of
   [] -> printf "【%s】\t該当キーワードがありません" name
   xs -> printf "【%s】\t" name ++ intercalate "/" (map suggestion xs)
-  where suggestion i = printf "%s[%d]" (pickOne npc i) i
+  where suggestion i = let i' = i -1
+                       in printf "%s[%d]" (pickOne npc i') i'
