45a154a68439fd9437bfd11da282dad3030d0992
sandmark <sandmark.m@gmail.com>
2016-01-15 08:25:46 +0900

    Fix `suggest` works with keyword arguments.

--- src/REPL/Suggest.hs
+++ src/REPL/Suggest.hs
@@ -2,10 +2,11 @@ module REPL.Suggest (suggest) where
 
 import Text.Printf
 import Data.List (intercalate)
+import Data.Maybe (fromJust)
 
 import REPL.NPC (npcLookup)
 import Rating
-import AlbanKnights (findKeyIndices, pickOne)
+import AlbanKnights (findKeyIndices, pickOne, getKeywordIndex)
 
 suggest :: [String] -> Rating -> Either String Rating
 suggest [str] r = case npcLookup str of
@@ -22,7 +23,16 @@ suggest [] r = Left $ intercalate "\n" $ map wrapToLine table
                 ,("elsie",  "エルシィ",   elsie r)]
         wrapToLine (npc, name, info) = toLine npc name info
 
-suggest (_:_) _ = Left "使い方: suggest NPC"
+suggest (str:keywords) _
+  | Nothing `elem` indices = Left "不正なキーワードが含まれています"
+  | otherwise              = case npcLookup str of
+      "dai"    -> Left $ toLine "dai"    "ダイ"       (Nothing, False, toStock indices)
+      "kaour"  -> Left $ toLine "kaour"  "カオル"     (Nothing, False, toStock indices)
+      "eirlys" -> Left $ toLine "eirlys" "アイリース" (Nothing, False, toStock indices)
+      "elsie"  -> Left $ toLine "elsie"  "エルシィ"   (Nothing, False, toStock indices)
+      _        -> Left "そのようなNPCは存在しません"
+  where indices = map getKeywordIndex keywords
+        toStock = map fromJust
 
 toLine :: String -> String-> Info -> String
 toLine _ name (_,_,[]) = printf "【%s】\tストックが空です" name
