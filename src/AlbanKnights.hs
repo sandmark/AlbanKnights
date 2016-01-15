5be6e53888b0aa5e11287761ea35b00be030f21f
sandmark <sandmark.m@gmail.com>
2016-01-15 10:15:13 +0900

    Fix `findKeyIndices` to return correct indices by given list length.

--- src/AlbanKnights.hs
+++ src/AlbanKnights.hs
@@ -16,7 +16,7 @@ findKeyIndices name list = finder 0
   where count = length list
         xs    = fromJust $ lookup name table
         finder i  | i > 99 = []
-                  | keys == list = i : finder (i+1)
+                  | keys == list = i+count : finder (i+1)
                   | otherwise = finder (i+1)
           where keys = take count $ drop i xs
 
